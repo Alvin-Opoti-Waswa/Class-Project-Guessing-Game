0791c20f769cc1c32da6fe716b6a1dcaf68b5ff2
Opoti Alvin Waswa <150916755+Alvin-Opoti-Waswa@users.noreply.github.com>
2024-01-21 11:43:25 +0100

    Update Project 102.py
    I have updated the guessing game to include if function, while loops, booleans, modules, functions and dictionaries.

--- Project 101.py
+++ Project 101.py
@@ -1,27 +1,41 @@
 import random
 
-correct_number = random.randint(1, 10)
+correct_number = random.randint(1, 100)
 
 print("Welcome to the guessing game!")
 
 def my_guessing_game():
+
     trials = 5
-    print("All you have to do is guess correctly the number that I picked between 1 and 100 but you only have 5 tries...BEGIN")
-    while trials > 0:        
-        attempt = int(input("Enter your best guess: "))
-        if attempt == correct_number:
-            print("We have our winner!!!!!.")
+
+    game_running = True
+  
+    player_info = {'name': '', 'trials': 0, 'correct_number': False}
+  
+    player_info['name'] = input("Enter your name: ")
+  
+    print(f"Hi, {player_info['name']}! I have selected a number between 1 and 100. Can you guess it?")
+
+    while game_running:
+        trial = int(input("Enter your attempt: "))
+        if trial == correct_number:
+            print(f"Congratulations, {player_info['name']}! You guessed the correct number.")
+            player_info['correct_number'] = True
             break
         else:
-            print("Sorry,try again.")
+            print("Incorrect guess. Try again.")
             trials -= 1
+            player_info['trials'] += 1
             if trials > 0:
-                print(f"You have {trials} {'trials' if trials > 1 else 'try'} remaining.")
-    if trials == 0:
-        print(f"Sadly, you run out of trials.The correct number was {correct_number}.")
+                print(f"You have {trials} {'attempts' if trials > 1 else 'trials'} remaining.")
+            else:
+                print(f"Sorry, {player_info['name']}, you've run out of trials. The correct number was {correct_number}.")
+                break
 
+    
+    print("\nPlayer Information:")
+    print(f"Name: {player_info['name']}")
+    print(f"Attempts: {player_info['trials']}")
+    print("Game Status: " + ("Win" if player_info['correct_number'] else "Loss"))
 
 my_guessing_game()
-
-
-
